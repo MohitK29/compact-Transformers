83c9e0cbeaee00c2b50338d48853c0d6e516390c
DESKTOP-E7H137C\mohit <mohitkewlani1448@gmail.com>
2022-04-22 15:04:40 -0400

    update

--- train.py
+++ train.py
@@ -77,8 +77,8 @@ parser.add_argument('-c', '--config', default='', type=str, metavar='FILE',
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 
 # Dataset / Model parameters
-parser.add_argument('data_dir', default= './data', metavar='DIR',
-                    help='path to dataset')
+# parser.add_argument('data_dir', default= './data', metavar='DIR',
+#                     help='path to dataset')
 parser.add_argument('--dataset', '-d', metavar='NAME', default='',
                     help='dataset type (default: ImageFolder/ImageTar if empty)')
 parser.add_argument('--train-split', metavar='NAME', default='train',
@@ -152,7 +152,7 @@ parser.add_argument('--warmup-lr', type=float, default=0.0001, metavar='LR',
                     help='warmup learning rate (default: 0.0001)')
 parser.add_argument('--min-lr', type=float, default=1e-5, metavar='LR',
                     help='lower lr bound for cyclic schedulers that hit 0 (1e-5)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--epochs', type=int, default=1200, metavar='N',
                     help='number of epochs to train (default: 2)')
 parser.add_argument('--epoch-repeats', type=float, default=0., metavar='N',
                     help='epoch repeat multiplier (number of times to repeat dataset epoch per train epoch).')
@@ -475,31 +475,31 @@ def main():
         _logger.info('Scheduled epochs: {}'.format(num_epochs))
 
      #create the train and eval datasets
-    dataset_train = create_dataset(
-        args.dataset,
-        root=args.data_dir, split=args.train_split, is_training=True,
-        batch_size=args.batch_size, repeats=args.epoch_repeats)
-    dataset_eval = create_dataset(
-        args.dataset, root=args.data_dir, split=args.val_split, is_training=False, batch_size=args.batch_size)
+    # dataset_train = create_dataset(
+    #     args.dataset,
+    #     root=args.data_dir, split=args.train_split, is_training=True,
+    #     batch_size=args.batch_size, repeats=args.epoch_repeats)
+    # dataset_eval = create_dataset(
+    #     args.dataset, root=args.data_dir, split=args.val_split, is_training=False, batch_size=args.batch_size)
 
-    #transform_train = transforms.Compose([
-    #    transforms.RandomCrop(32, padding=4),
-    #    transforms.RandomHorizontalFlip(),
-    #    transforms.ToTensor(),
-    #    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    #])
+    transform_train = transforms.Compose([
+       transforms.RandomCrop(32, padding=4),
+       transforms.RandomHorizontalFlip(),
+       transforms.ToTensor(),
+       transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-    #transform_test = transforms.Compose([
-    #    transforms.ToTensor(),
-    #    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    #])
+    transform_test = transforms.Compose([
+       transforms.ToTensor(),
+       transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-    #dataset_train = torchvision.datasets.CIFAR100(
-    #root='./cifar100', train=True, download=True, transform=transform_train)
+    dataset_train = torchvision.datasets.CIFAR10(
+    root='./cifar10', train=True, download=True, transform=transform_train)
     #loader_train = torch.utils.data.DataLoader(dataset_train, batch_size=128, shuffle=True, num_workers=2)
 
-    #dataset_eval = torchvision.datasets.CIFAR100(
-    #root='./cifar100', train=False, download=True, transform=transform_test)
+    dataset_eval = torchvision.datasets.CIFAR10(
+    root='./cifar10', train=False, download=True, transform=transform_test)
     #loader_eval = torch.utils.data.DataLoader(dataset_eval, batch_size=100, shuffle=False, num_workers=2)
 
     # setup mixup / cutmix
@@ -839,27 +839,27 @@ def validate(model, loader, loss_fn, args, amp_autocast=suppress, log_suffix='')
 
 def loss_acc_plot():
     fig1 = plt.figure(figsize=(8, 8))
-    plt.plot(range(310), train_loss_history, '-',
+    plt.plot(range(1500), train_loss_history, '-',
             linewidth=3, label='Train error')
-    plt.plot(range(310), val_loss_history, '-',
+    plt.plot(range(1500), val_loss_history, '-',
             linewidth=3, label='Val error')
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.grid(True)
     plt.legend()
-    plt.savefig('experiment_imgnet_loss.png')
+    plt.savefig('cifar10_1500_epochs_loss.png')
     plt.show()
 
     fig2 = plt.figure(figsize=(8, 8))
     # plt.plot(len(train_acc_history), train_acc_history, '-',
     #         linewidth=3, label='Train accuracy')
-    plt.plot(range(310), val_acc_history, '-',
+    plt.plot(range(1500), val_acc_history, '-',
             linewidth=3, label='Val accuracy')
     plt.xlabel('epoch')
     plt.ylabel('acc')
     plt.grid(True)
     plt.legend()
-    plt.savefig('experiment_imgnet_acc.png')
+    plt.savefig('cifar10_1500_epochs_acc.png')
     plt.show()
 
 if __name__ == '__main__':
