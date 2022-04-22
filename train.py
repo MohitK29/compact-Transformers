7a9ec35d7be9200d0b4bbcc2ef7d78a8fa57e184
DESKTOP-E7H137C\mohit <mohitkewlani1448@gmail.com>
2022-04-22 03:48:24 -0400

    updated loader.

--- train.py
+++ train.py
@@ -496,9 +496,11 @@ def main():
 
     dataset_train = torchvision.datasets.Flowers102(
     root='./flowers102', split = 'train', download=True, transform=transform_train)
+    loader_train = torch.utils.data.DataLoader(dataset_train, batch_size=128, shuffle=True, num_workers=2)
 
     dataset_eval = torchvision.datasets.Flowers102(
     root='./flowers102', split = 'val', download=True, transform=transform_test)
+    loader_eval = torch.utils.data.DataLoader(dataset_eval, batch_size=100, shuffle=False, num_workers=2)
 
     # setup mixup / cutmix
     collate_fn = None
@@ -523,48 +525,49 @@ def main():
     train_interpolation = args.train_interpolation
     if args.no_aug or not train_interpolation:
         train_interpolation = data_config['interpolation']
-    loader_train = create_loader(
-        dataset_train,
-        input_size=data_config['input_size'],
-        batch_size=args.batch_size,
-        is_training=True,
-        use_prefetcher=args.prefetcher,
-        no_aug=args.no_aug,
-        re_prob=args.reprob,
-        re_mode=args.remode,
-        re_count=args.recount,
-        re_split=args.resplit,
-        scale=args.scale,
-        ratio=args.ratio,
-        hflip=args.hflip,
-        vflip=args.vflip,
-        color_jitter=args.color_jitter,
-        auto_augment=args.aa,
-        num_aug_splits=num_aug_splits,
-        interpolation=train_interpolation,
-        mean=data_config['mean'],
-        std=data_config['std'],
-        num_workers=args.workers,
-        distributed=args.distributed,
-        collate_fn=collate_fn,
-        pin_memory=args.pin_mem,
-        use_multi_epochs_loader=args.use_multi_epochs_loader
-    )
 
-    loader_eval = create_loader(
-        dataset_eval,
-        input_size=data_config['input_size'],
-        batch_size=args.validation_batch_size_multiplier * args.batch_size,
-        is_training=False,
-        use_prefetcher=args.prefetcher,
-        interpolation=data_config['interpolation'],
-        mean=data_config['mean'],
-        std=data_config['std'],
-        num_workers=args.workers,
-        distributed=args.distributed,
-        crop_pct=data_config['crop_pct'],
-        pin_memory=args.pin_mem,
-    )
+    # loader_train = create_loader(
+    #     dataset_train,
+    #     input_size=data_config['input_size'],
+    #     batch_size=args.batch_size,
+    #     is_training=True,
+    #     use_prefetcher=args.prefetcher,
+    #     no_aug=args.no_aug,
+    #     re_prob=args.reprob,
+    #     re_mode=args.remode,
+    #     re_count=args.recount,
+    #     re_split=args.resplit,
+    #     scale=args.scale,
+    #     ratio=args.ratio,
+    #     hflip=args.hflip,
+    #     vflip=args.vflip,
+    #     color_jitter=args.color_jitter,
+    #     auto_augment=args.aa,
+    #     num_aug_splits=num_aug_splits,
+    #     interpolation=train_interpolation,
+    #     mean=data_config['mean'],
+    #     std=data_config['std'],
+    #     num_workers=args.workers,
+    #     distributed=args.distributed,
+    #     collate_fn=collate_fn,
+    #     pin_memory=args.pin_mem,
+    #     use_multi_epochs_loader=args.use_multi_epochs_loader
+    # )
+
+    # loader_eval = create_loader(
+    #     dataset_eval,
+    #     input_size=data_config['input_size'],
+    #     batch_size=args.validation_batch_size_multiplier * args.batch_size,
+    #     is_training=False,
+    #     use_prefetcher=args.prefetcher,
+    #     interpolation=data_config['interpolation'],
+    #     mean=data_config['mean'],
+    #     std=data_config['std'],
+    #     num_workers=args.workers,
+    #     distributed=args.distributed,
+    #     crop_pct=data_config['crop_pct'],
+    #     pin_memory=args.pin_mem,
+    # )
 
     # setup loss function
     if args.jsd:
